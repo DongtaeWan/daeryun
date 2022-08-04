4d82dbc1c958f9a41ef0b65167307e24754c357f
Dong <bdj42@naver.com>
2022-08-04 17:36:09 +0900

    feat: 형사전문센터 메인

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,41 +1,106 @@
+import Link from "next/link";
 import React from "react";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.header_left}>
-        <a href="" className={styles.logo}>
-          logo
-        </a>
+        <Link href="/">
+          <img src="/images/logo.png" alt="형사전문센터" />
+        </Link>
       </div>
       <div className={styles.header_middle}>
         <ul>
           <li>
-            <a href="">형사전문센터</a>
+            <Link href="/">형사전문센터</Link>
+            <ul>
+              <li>
+                <Link href="/">센터 소개</Link>
+              </li>
+              <li>
+                <Link href="/">법률 정보</Link>
+              </li>
+              <li>
+                <Link href="/">오시는 길</Link>
+              </li>
+              <li>
+                <Link href="/">공식 홈페이지</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">형사전문변호사</a>
+            <Link href="/">형사전문변호사</Link>
+            <ul>
+              <li>
+                <Link href="/">구성원 소개</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">성공사례</a>
+            <Link href="/">성공사례</Link>
+            <ul>
+              <li>
+                <Link href="/">성공사례</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">업무분야</a>
+            <Link href="/">업무분야</Link>
+            <ul>
+              <li>
+                <Link href="/">재산범죄</Link>
+              </li>
+              <li>
+                <Link href="/">강력범죄</Link>
+              </li>
+              <li>
+                <Link href="/">교통범죄</Link>
+              </li>
+              <li>
+                <Link href="/">보이스피싱</Link>
+              </li>
+              <li>
+                <Link href="/">학교폭력</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">언론보도</a>
+            <Link href="/">언론보도</Link>
+            <ul>
+              <li>
+                <Link href="/">언론보도</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">고객후기</a>
+            <Link href="/">고객후기</Link>
+            <ul>
+              <li>
+                <Link href="/">고객후기</Link>
+              </li>
+            </ul>
           </li>
           <li>
-            <a href="">전문법률상담</a>
+            <Link href="/">전문법률상담</Link>
+            <ul>
+              <li>
+                <Link href="/">전문법률상담예약</Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
-      <div className={styles.header_ight}>
-        <img src="../public/images/ccenter1.png" alt="" />
+      <div className={styles.header_right}>
+        <div>
+          <div className={styles.header_call_left}>
+            <span>상담예약</span>
+          </div>
+          <div className={styles.header_call_right}>
+            <img src="/images/top_tel.png" alt="" />
+            <span>1800-7905</span>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ components/section/Section1.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Section1.module.scss";
+
+const Section1 = () => {
+  return (
+    <>
+      <section className={styles.section1}>
+        <div className={styles.section1_top}>
+          <p>검찰&#183;경찰&#183;법원 전관출신 형사전문변호인단</p>
+        </div>
+        <div className={styles.section1_bottom}>
+          <div className={styles.section1_content1}>
+            <img src="/images/main_1.png" alt="" />
+            <p>대검찰청&#183;고검</p>
+            <p>차장검사출신</p>
+          </div>
+          <div className="section1_content2">
+            <img src="/images/main_2.png" alt="" />
+            <p>경찰간부출신</p>
+            <p>전문위원</p>
+          </div>
+          <div className="section1_content3">
+            <img src="/images/main_3.png" alt="" />
+            <p>지방&#183;고등법원</p>
+            <p>재판연구원출신</p>
+          </div>
+          <div className="section1_content4">
+            <img src="/images/main_4.png" alt="" />
+            <p>대한변호사협회</p>
+            <p>전문분야등록</p>
+          </div>
+        </div>
+
+        <div className={styles.mause}>
+          <div class="wheel"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Section1;
